ac8e028682ad93d8b1567e4e21e9c154e82e84f7
Robin Henry <robin.henry012@gmail.com>
2018-03-10 23:27:11 +0000

    Finally

---
+++ test2.py
@@ -0,0 +1,52 @@
+from doomObject import DoomObject
+import unittest
+import math
+
+testObj = DoomObject ({
+    "id": 1,
+    'position': {
+        "x": 10,
+        "y": 10,
+        "z": 0
+    },
+    "height": 1,
+    "angle": 0,
+    "health": 100,
+    "type": 1,
+    "distance": 200
+})
+
+def angleToPos(obj, x, y):
+    dx = x - obj.x
+    dy = y - obj.y
+    dis = math.sqrt(dx*dx + dy*dy)
+    # 180 < alpha < 0 -> angle between x=0 and position (x,y)
+    if (dy > 0):
+        alpha = math.acos(dx / dis) * 180 / math.pi
+    else:
+        alpha = - math.acos(dx / dis) * 180 / math.pi
+    print ("alpha: " + str(alpha))
+
+    angleToTurn = (alpha - obj.angle) % 360
+    print("angleToTurn: " + str(angleToTurn))
+
+    if (angleToTurn > 180):
+        result =  angleToTurn - 360
+    elif (angleToTurn < -180):
+        result = 360 + angleToTurn
+    else:
+        result = angleToTurn
+
+    print("Result: " + str(result))
+    return result
+
+
+
+
+def findCoordinates(obj, angle, distance):
+    alpha = (math.pi/180) * (obj.angle + angle)
+    x = obj.x + distance * math.cos(alpha)
+    y = obj.y + distance * math.sin(alpha)
+    return int(round(x)), int(round(y))
+
+print("Answer: " + str(angleToPos(testObj, -10, -10 )))
