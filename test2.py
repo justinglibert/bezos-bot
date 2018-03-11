d966448a24551c400a8a2b53020256aaa2d32ae6
Robin Henry <robin.henry012@gmail.com>
2018-03-11 09:04:20 +0000

    getDistance

--- test2.py
+++ test2.py
@@ -1,16 +1,17 @@
 from doomObject import DoomObject
 import unittest
 import math
+import numpy.linalg
 
 testObj = DoomObject ({
     "id": 1,
     'position': {
-        "x": 10,
-        "y": 10,
+        "x": 3,
+        "y": 0,
         "z": 0
     },
     "height": 1,
-    "angle": 0,
+    "angle": 45,
     "health": 100,
     "type": 1,
     "distance": 200
@@ -27,10 +28,8 @@ def angleToPos(obj, x, y):
         alpha = math.acos(dx / dis) * 180 / math.pi
     else:
         alpha = - math.acos(dx / dis) * 180 / math.pi
-    print ("alpha: " + str(alpha))
 
     angleToTurn = (alpha - obj.angle) % 360
-    print("angleToTurn: " + str(angleToTurn))
 
     if (angleToTurn > 180):
         result =  angleToTurn - 360
@@ -41,13 +40,28 @@ def angleToPos(obj, x, y):
 
     return result
 
-
-
-
 def findCoordinates(obj, angle, distance):
     alpha = (math.pi/180) * (obj.angle + angle)
     x = obj.x + distance * math.cos(alpha)
     y = obj.y + distance * math.sin(alpha)
     return int(round(x)), int(round(y))
 
-print("Answer: " + str(angleToPos(testObj, -10, -10 )))
+
+# dist < 0 = go to the left, dist > 0 = go to the right
+def getDistance(obj, x1, y1):
+    if obj.angle == 0:
+        return obj.x - x1
+    elif obj.angle == 180:
+        return x1 - obj.x
+    a = math.tan((math.pi/180) * obj.angle )
+    b = -1
+    c = y1 - a * x1
+    print("a: " + str(a))
+    if a == 0:
+        return None
+    else:
+        dist = a * obj.x + b * obj.y + c / math.sqrt(a*a + b*b)
+    if obj.angle < 180:
+        return dist
+    else:
+        return -dist
